8d77a2bd7e0ceb24109fef3f5239e2e69d774622
Igor Popov <popov.nirvana@gmail.com>
2021-05-04 08:54:19 +0300

    Hello world!

--- TestProcess/main.cpp
+++ TestProcess/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <conio.h>
+#include <windows.h>
+
+class StaticClass
+{
+public:
+	StaticClass ()
+	{
+		std::cout << "Static object constructed.\n";
+	}
+	~StaticClass ()
+	{
+		std::cout << "Static object destructed.\n";
+	}
+};
+
+StaticClass sobj;
 
 int main (int argc, char* argv [])
 {
-	std::cout << "Hello world!";
+	std::cout << "Hello world!\n";
+	_getch ();
 	return 0;
 }
